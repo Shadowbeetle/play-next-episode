94b8985f4350b10d1795870a16a32c0bb38a4321
nazgul <tamas.kadlecsik@tgac.ac.uk>
2016-03-05 18:20:11 +0100

    cleanup

--- play-next-episode.sh
+++ play-next-episode.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
+
 cd /home/nazgul/Prog/Home/PlayNextEpisode/
-
-rm vlc.log pid
-
 python play_next_episode.py
-
-sleep 1
-
-
-# TODO try moving this to python
-#tail -fn0 vlc.log | while read line ; do
-#        echo "$line" | grep "starting playback of the new playlist item"
-#        if [ $? = 0 ]
-#        then
-#                PID=$(<pid)
-#                kill -15 $PID
-#                rm vlc.log pid
-#                bash play_next_episode.sh
-#        fi
-#done
